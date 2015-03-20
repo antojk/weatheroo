972811ab1dff56bdf5a1148f2741adc003fa91b9
Antony John <antojk@gmail.com>
2015-03-20 08:19:49 +0530

    Added basic weather display

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@
  */
 
 var UI = require('ui');
+var Ajax = require('ajax');
+
+
 // Initialize a card face
-var card = new UI.Card({title:'Weather',subtitle:'Fetching...'});
+var card = new UI.Card(
+  {
+    title:'Weather',
+    subtitle:'Fetching...'
+  }
+);
+
 // Display the card
 card.show();
+
+var cityName = 'Bangalore', 
+    URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+Ajax(
+  {
+    url:URL,
+    type:'json'
+  },
+  function(data){
+    //Success
+    console.log('Successfully fetched data!');
+    
+    var location = data.name, 
+        temperature = Math.round(data.main.temp - 273.15) + 'C',
+        description = data.weather[0].description;
+    description = description.charAt(0).toUpperCase() + description.substring(1);
+    
+    card.subtitle(location + ', ' + temperature);
+    card.body(description);
+    
+    
+  },
+  function(error){
+    console.log('Failed fetching weather data: ' + error);
+  }
+);
+
