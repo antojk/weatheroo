960fe7327061cac84d1fc6957c96eeab9bd5fc89
Antony John <antojk@gmail.com>
2015-03-20 07:52:41 +0530

    Initialized
    Created my first code base for a pebble watch app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+// Initialize a card face
+var card = new UI.Card({title:'Weather',subtitle:'Fetching...'});
+// Display the card
+card.show();
